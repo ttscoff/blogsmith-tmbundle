4b8a299cbf7faa0b3fcfcd95309db5be8626945b
Brett Terpstra <me@brettterpstra.com>
2009-10-10 07:43:21 -0500

    Trying a little hud for statistics notification.

--- Support/lib/cooldialog.rb
+++ Support/lib/cooldialog.rb
@@ -3,19 +3,32 @@ module TextMate
   module CoolDialog
 	
 	class << self
+		def notification_hud(options)
+	        raise if options.empty?
+
+	        support = ENV['TM_BUNDLE_SUPPORT']
+	        nib     = support + '/nibs/notification_hud.nib'
+
+	        plist = Hash.new
+	        plist['title']    = options[:title]   || ''
+	        plist['summary']  = options[:summary] || ''
+	        plist['log']      = options[:log]     || ''
+
+	        `#{TM_DIALOG} -cqp #{e_sh plist.to_plist} #{e_sh nib} &> /dev/null &`
+      	end
 		def has_dialog2
 			tm_dialog = e_sh ENV['DIALOG']
 			! tm_dialog.match(/2$/).nil? 
 		end
 		def cool_tool_tip(content)
 			if has_dialog2
-			output = %^<div style="background: #666;">^
-			output += content.gsub(/([^\n+]+)\n/m,"<p>\\1</p>")
+			output = %^<div style="background: #666;" id="css1">^
+			output += content.gsub(/^([^\n+]+)\n/m,"<p>\\1</p>")
 			output += %^</div>^
 			html = <<-HTML
 			<head>
 			<style type="text/css" media="screen">
-			body{padding:1em;max-width:400px;}
+			body{padding:1em;}
 			#css0 {
 			  -webkit-transition: all 0.3s ease-in;
 			  -webkit-transform: scale(0.5);
@@ -27,10 +40,10 @@ module TextMate
 				text-shadow: 1px 1px 2px #000;
 				-webkit-box-shadow: 0.2em 0.3em 1em #000;
 				-webkit-border-radius: 1em;
-				line-height: 2.5em;
+				line-height: 1.5em;
 				padding: 0 1em;
 				float: left;
-				-webkit-box-shadow: -4px 4px 1px #fff;
+				-webkit-box-shadow: -4px 4px 1px #ccc;
 			}
 			</style>
 			</head>
